309cf388f144156ed32b1c9c9c123dd81046ca24
Sanowar Dewan <sanowardwnofficial@gmail.com>
2023-11-05 05:10:58 +0600

    comments added

--- teamWallet.sol
+++ teamWallet.sol
@@ -6,7 +6,7 @@ contract TeamWallet {
     uint memberCount; // Count of team members
     mapping(uint => address) membersById; // Mapping of member IDs to addresses
     uint creditsAvailable;
-    
+
     struct Transaction {
         uint amount;
         uint approvals;
@@ -14,14 +14,17 @@ contract TeamWallet {
         bool executed;
         bool failed;
     }
-    
+
     Transaction[] transactions;
     mapping(address => mapping(uint => bool)) hasApprovedOrRejected;
 
+    // Modifier to restrict access to only the deployer
     modifier onlyDeployer() {
         require(msg.sender == deployer, "Only the deployer can call this function");
         _;
     }
+
+    // Function to check if an address is a team member
     function isMember(address _address) internal view returns (bool) {
         for (uint i = 1; i <= memberCount; i++) {
             if (membersById[i] == _address) {
@@ -31,11 +34,13 @@ contract TeamWallet {
         return false;
     }
 
+    // Modifier to restrict access to team members
     modifier onlyMembers() {
         require(isMember(msg.sender), "Only team members can call this function");
         _;
     }
 
+    // Modifier to check if a transaction with a given index exists
     modifier transactionExists(uint n) {
         require(n < transactions.length, "Transaction does not exist");
         _;
@@ -46,6 +51,7 @@ contract TeamWallet {
         memberCount = 0; // Initialize member count to 0
     }
 
+    // Function to set up the wallet with team members and initial credits
     function setWallet(address[] memory _members, uint _credits) public onlyDeployer {
         require(_members.length > 0, "At least one member is required");
         require(_credits > 0, "Credits must be greater than 0");
@@ -60,6 +66,7 @@ contract TeamWallet {
         creditsAvailable = _credits;
     }
 
+    // Function to propose a spending transaction
     function spend(uint _amount) public onlyMembers {
         require(_amount > 0, "Amount must be greater than 0");
 
@@ -67,6 +74,7 @@ contract TeamWallet {
         transactions.push(Transaction(_amount, 1, 0, false, false));
     }
 
+    // Function to approve a spending transaction
     function approve(uint _n) public onlyMembers transactionExists(_n) {
         require(!hasApprovedOrRejected[msg.sender][_n], "Already approved or rejected");
         require(!transactions[_n].executed, "Transaction already executed");
@@ -87,6 +95,7 @@ contract TeamWallet {
         }
     }
 
+    // Function to reject a spending transaction
     function reject(uint _n) public onlyMembers transactionExists(_n) {
         require(!hasApprovedOrRejected[msg.sender][_n], "Already approved or rejected");
         require(!transactions[_n].executed, "Transaction already executed");
@@ -100,10 +109,12 @@ contract TeamWallet {
         }
     }
 
+    // Function to check available credits
     function credits() public view onlyMembers returns (uint) {
         return creditsAvailable;
     }
 
+    // Function to view transaction details
     function viewTransaction(uint _n) public view onlyMembers transactionExists(_n) returns (uint amount, string memory status) {
         Transaction storage transaction = transactions[_n];
 
@@ -124,23 +135,24 @@ contract TeamWallet {
         amount = transaction.amount;
     }
 
+    // Function to get transaction statistics
     function transactionStats() public view onlyMembers returns (uint debitedCount, uint pendingCount, uint failedCount) {
-    for (uint i = 0; i < transactions.length; i++) {
-        Transaction storage transaction = transactions[i];
+        for (uint i = 0; i < transactions.length; i++) {
+            Transaction storage transaction = transactions[i];
 
-        if (transaction.executed) {
-            if (transaction.failed) {
+            if (transaction.executed) {
+                if (transaction.failed) {
+                    failedCount++;
+                } else {
+                    debitedCount++;
+                }
+            } else if (transaction.approvals >= (memberCount * 70) / 100) {
+                pendingCount++;
+            } else if (transaction.rejections > (memberCount * 30) / 100) {
                 failedCount++;
             } else {
-                debitedCount++;
+                pendingCount++;
             }
-        } else if (transaction.approvals >= (memberCount * 70) / 100) {
-            pendingCount++;
-        } else if (transaction.rejections > (memberCount * 30) / 100) {
-            failedCount++;
-        } else {
-            pendingCount++;
         }
     }
-}
 }
